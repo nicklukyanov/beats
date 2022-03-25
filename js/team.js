271c304db911bfabca6d02385923908a9d05af96
nicklukyanov <97785901+nicklukyanov@users.noreply.github.com>
2022-03-25 11:13:05 +0300

    Add files via upload

---
+++ js/team.js
@@ -0,0 +1,20 @@
+let list = document.getElementById("teamList");
+
+const nodeCollect = document.querySelectorAll(".team__name");
+const htmlCollect = document.getElementsByClassName("team__name");
+const arr = [1, 2, 3, 4];
+
+arr.forEach(function name(element) {
+  console.log(element);
+})
+
+
+nodeCollect.forEach(function name(element) {
+  console.log(element);
+})
+
+htmlCollect.forEach(function name(element) {
+  console.log(element);
+})
+
+
