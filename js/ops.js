c5062e1bb67d57e210b3085ba96faf381eccd36d
nicklukyanov <97785901+nicklukyanov@users.noreply.github.com>
2022-04-08 00:34:45 +0300

    Загрузил задание 5-ой недели

---
+++ js/ops.js
@@ -0,0 +1,120 @@
+const sections = $('section');
+const display = $('.maincontent');
+
+
+
+let inScroll = false;
+
+sections.first().addClass("active");
+
+const performTransition = (sectionEq) => {
+
+  if (inScroll === false) {
+    inScroll = true;
+    const position = sectionEq * -100;
+
+    const currentSection = sections.eq(sectionEq);
+    const menuTheme = currentSection.attr("data-sidemenu-theme");
+    const sidemenu = $(".fixed-menu");
+
+    if (menuTheme == "black") {
+      sidemenu.addClass("fixed-menu--shadowed");
+    } else {
+      sidemenu.removeClass("fixed-menu--shadowed");
+    }
+
+    display.css({
+      transform: `translateY(${position}%)`,
+    });
+
+    sections.eq(sectionEq).addClass("active").siblings().removeClass("active");
+
+    sidemenu
+      .find(".fixed-menu__item")
+      .eq(sectionEq)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+
+    setTimeout(() => {
+      inScroll = false;
+
+    }, 1300);
+  }
+
+};
+
+const scrollViewport = (direction) => {
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+
+  if (direction === "next" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+}
+
+$(window).on('wheel', (e) => {
+  const deltaY = e.originalEvent.deltaY;
+
+  if (deltaY > 0) {
+    scrollViewport("next");
+  }
+
+  if (deltaY < 0) {
+    scrollViewport("prev");
+  }
+});
+
+$(window).on("keydown", (e) => {
+
+  const tagName = e.target.tagName.toLowerCase();
+
+  if (tagName !== "input" && tagName !== "textarea") {
+    switch (e.keyCode) {
+      case 38: //prev
+        scrollViewport("prev")
+        break;
+
+      case 40: //next
+        scrollViewport("next")
+        break;
+    }
+  }
+
+});
+
+$(".wrapper").on("touchmove", e => e.preventDefault());
+
+$("[data-scroll-to]").click(e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-scroll-to");
+  const reqsection = $(`[data-section-id=${target}]`);
+
+  performTransition(reqsection.index());
+
+});
+
+
+const mobileDetect = new mobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+if (isMobile) {
+  $("body").swipe({
+    swipe: function (event, direction) {
+      const scroller = viewportScroller();
+      let scrollDirection = "";
+
+      if (direction == "up") scrollDirection = "next";
+      if (direction == "down") scrollDirection = "prev";
+      scroller[scrollDirection]();
+    },
+  });
+}
+
