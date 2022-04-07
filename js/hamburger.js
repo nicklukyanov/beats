ee8485a5bca1195e96f9b9b50fbb607a6afe7e16
nicklukyanov <97785901+nicklukyanov@users.noreply.github.com>
2022-04-07 18:42:36 +0300

    Delete js directory

--- js/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', (e) => {
-  e.preventDefault();
-  hamburger.classList.toggle('hamburger--active');
-  menu.classList.toggle('menu--active');
-});
